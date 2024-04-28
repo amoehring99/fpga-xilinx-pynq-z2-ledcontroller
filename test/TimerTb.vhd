bb02657506c238c0e4226a0d81902bb20b3a869c
Alexander Möhring <urgaj@student.kit.edu>
2024-04-28 16:12:32 +0200

    added template for timer module and its benchmark

---
+++ test/TimerTb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/28/2024 04:08:26 PM
+-- Design Name: 
+-- Module Name: TimerTb - sim
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TimerTb is
+--  Port ( );
+end TimerTb;
+
+architecture sim of TimerTb is
+
+begin
+
+
+end sim;
