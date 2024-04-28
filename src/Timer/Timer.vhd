4e43ee27f64773d6b96c58fd7df0bb3654374ae8
Alexander Möhring <urgaj@student.kit.edu>
2024-04-28 17:59:31 +0200

    implemented timer to count seconds, minutes and houres based on generic clock frequency in hz

--- src/Timer/Timer.vhd
+++ src/Timer/Timer.vhd
@@ -1,43 +1,102 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 04/28/2024 04:06:38 PM
--- Design Name: 
+-- Design Name:
 -- Module Name: Timer - rtl
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+  use ieee.std_logic_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+-- use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- library UNISIM;
+-- use UNISIM.VComponents.all;
 
-entity Timer is
---  Port ( );
-end Timer;
+entity timer is
+  generic (
+    clock_frequency_hz : integer
+  );
+  port (
+    clk     : in    std_logic;
+    n_rst   : in    std_logic;
+    seconds : inout integer;
+    minutes : inout integer;
+    houres  : inout integer
+  );
+end entity timer;
 
-architecture rtl of Timer is
+architecture rtl of timer is
+
+  signal ticks : integer;
+
+  procedure increment_wrap (
+    -- count up ticks / seconds / minutes ...
+    signal counter : inout integer;
+    -- wrap when next higher time value is reached,
+    -- e.g. after 60 seconds
+    constant wrap_value : in integer;
+    -- only enable when next lower time value was reached
+    -- e.g. increase minute if seconds wrapped
+    constant enable : in boolean;
+    -- trigger if counter has wrapped
+    variable wrapped : out boolean
+  ) is
+  begin
+
+    wrapped := false;
+
+    if (enable) then
+      if (counter < wrap_value - 1) then
+        counter <= counter + 1;
+      else
+        counter <= 0;
+        wrapped := true;
+      end if;
+    end if;
+
+  end procedure;
 
 begin
 
+  count_ticks : process (clk) is
 
-end rtl;
+    variable wrap : boolean;
+
+  begin
+
+    if rising_edge(clk) then
+      if (n_rst = '0') then
+        ticks   <= 0;
+        seconds <= 0;
+        minutes <= 0;
+        houres  <= 0;
+      else
+        -- clock frequency is ticks per second -> if this wraps, one second has passed
+        increment_wrap(ticks, clock_frequency_hz, true, wrap);
+        increment_wrap(seconds, 60, wrap, wrap);
+        increment_wrap(minutes, 60, wrap, wrap);
+        increment_wrap(houres, 24, wrap, wrap);
+      end if;
+    end if;
+
+  end process count_ticks;
+
+end architecture rtl;
