c3b9c507095cab3738d641da5cfb8abdf670bbc5
Alexander Möhring <urgaj@student.kit.edu>
2024-04-28 15:25:54 +0200

    added template files for LED functionalities

---
+++ src/LEDPattern.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/28/2024 03:24:15 PM
+-- Design Name: 
+-- Module Name: LEDPattern - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LEDPattern is
+--  Port ( );
+end LEDPattern;
+
+architecture rtl of LEDPattern is
+
+begin
+
+
+end rtl;
