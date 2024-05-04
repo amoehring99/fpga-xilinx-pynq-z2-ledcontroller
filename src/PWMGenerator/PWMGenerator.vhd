0ee3f289d61734be7e98c8ab57c83755eb99be9e
Alexander Möhring <urgaj@student.kit.edu>
2024-05-04 17:58:15 +0200

    added templates for pwm signal generator

---
+++ src/PWMGenerator/PWMGenerator.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/04/2024 02:57:14 PM
+-- Design Name: 
+-- Module Name: PWMGenerator - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PWMGenerator is
+--  Port ( );
+end PWMGenerator;
+
+architecture rtl of PWMGenerator is
+
+begin
+
+
+end rtl;
---
+++ test/PWMGenerator/PWMGenerator_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/04/2024 03:11:59 PM
+-- Design Name: 
+-- Module Name: PWMGenerator_tb - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PWMGenerator_tb is
+--  Port ( );
+end PWMGenerator_tb;
+
+architecture rtl of PWMGenerator_tb is
+
+begin
+
+
+end rtl;
